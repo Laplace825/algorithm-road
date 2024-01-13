262ba9b8c06a343a67ec458daea4e09377853274
Laplace <Laplace_yongle_he@163.com>
2024-01-13 10:37:51 +0800

    'findlongestSubsr'

--- Algorithm-Question-Solutions/find-longest-substring/FindLongSubStr.h
+++ Algorithm-Question-Solutions/find-longest-substring/FindLongSubStr.h
@@ -1,42 +1,45 @@
 #ifndef FIND_LONG_SUB_STR_H
 #define FIND_LONG_SUB_STR_H
 
-#include <iostream> 
+#include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
 
-
-std::string longestCommonSubstring(std::string s1, std::string s2) {
+std::string longestCommonSubstring(std::string s1, std::string s2)
+{
   // 初始化长度
   int n = s1.size();
   int m = s2.size();
-  
-  // dp 
+
+  // dp
   // 初始化dp数组，dp[i][j]表示s1[0:i-1]和s2[0:j-1]的最长公共子串长度
-  std::vector<std::vector<int> > dp(n+1, std::vector<int>(m+1, 0));
-  
+  std::vector<std::vector<int>> dp(n + 1, std::vector<int>(m + 1, 0));
+
   // 初始化最大长度和结束位置
   int maxLen = 0, endIndex = 0;
-  
+
   // O(m*n)
-  for(int i=0; i<n; i++) {
-    for(int j=0; j<m; j++) {
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < m; j++)
+    {
       // 如果当前字符相等
-      if(s1[i] == s2[j]) {  
+      if (s1[i] == s2[j])
+      {
         // dp[i+1][j+1]表示s1[0:i]和s2[0:j]的最长公共子串长度
-        dp[i+1][j+1] = 1 + dp[i][j];
+        dp[i + 1][j + 1] = 1 + dp[i][j];
         // 如果当前长度大于最大长度，更新最大长度和结束位置
-        if(dp[i+1][j+1] > maxLen) {
-            maxLen = dp[i+1][j+1];
-            endIndex = i+1;
+        if (dp[i + 1][j + 1] > maxLen)
+        {
+          maxLen = dp[i + 1][j + 1];
+          endIndex = i + 1;
         }
       }
     }
   }
-  
+
   // 返回最大长度对应的子串
-  return s1.substr(endIndex-maxLen, maxLen); 
+  return s1.substr(endIndex - maxLen, maxLen);
 }
 
 #endif // FIND_LONG_SUB_STR_H
--- Algorithm-Question-Solutions/find-longest-substring/main.cpp
+++ Algorithm-Question-Solutions/find-longest-substring/main.cpp
@@ -1,13 +1,13 @@
 
 #include "FindLongSubStr.h"
 #include <string>
-using std::string;
-using std::cout ;
 
-int main(int argc , char **argv)
+int main(int argc, char **argv)
 {
+    using std::cout;
+    using std::string;
     string s1 = "abaac";
     string s2 = "avbaacss";
-    string s3 = longestCommonSubstring(s1 ,s2);
+    string s3 = longestCommonSubstring(s1, s2);
     cout << s3 << '\n';
 }
