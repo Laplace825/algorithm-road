eae8aee59cf7c1fcd859b3554aff6e1b74d218c3
laplace825 <2959452323@qq.com>
2024-03-27 16:42:24 +0800

    update in Wed Mar 27 04:42:24 PM CST 2024

--- Data-Structure/diagram/diagram.cpp
+++ Data-Structure/diagram/diagram.cpp
@@ -3,9 +3,15 @@
 
 int main()
 {
-    auto graph = ds::get_graph_dire();
-    graph.print();
-    graph.dfs_trans(0);
-    graph.bfs_trans(0);
-    ds::Trans(graph, 0, ds::TransType::DFS);
+    auto dir_graph = ds::get_graph_dire();
+    dir_graph.print();
+    dir_graph.dfs_trans(0);
+    dir_graph.bfs_trans(0);
+    ds::Trans(dir_graph, 0, ds::TransType::DFS);
+    auto undir_graph = ds::get_graph_undire();
+    undir_graph.print();
+    undir_graph.dfs_trans(0);
+    undir_graph.bfs_trans(0);
+    ds::Trans(undir_graph, 0, ds::TransType::BFS);
+    std::cout << dir_graph.bfs_arc_exist(1, 3) << std::endl;
 }
