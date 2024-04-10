6011c1532b0b1af719c1bfe9e0868cd497a88c29
laplace825 <2959452323@qq.com>
2024-04-10 19:06:25 +0800

    update on Wed Apr 10 07:06:25 PM CST 2024

--- Data-Structure/diagram/diagram.cpp
+++ Data-Structure/diagram/diagram.cpp
@@ -4,14 +4,33 @@
 int main()
 {
     auto dir_graph = ds::get_graph_dire();
-    dir_graph.print();
-    dir_graph.dfs_trans(0);
-    dir_graph.bfs_trans(0);
-    ds::Trans(dir_graph, 0, ds::TransType::DFS);
-    auto undir_graph = ds::get_graph_undire();
-    undir_graph.print();
-    undir_graph.dfs_trans(0);
-    undir_graph.bfs_trans(0);
-    ds::Trans(undir_graph, 0, ds::TransType::BFS);
-    std::cout << dir_graph.bfs_arc_exist(1, 3) << std::endl;
+    for (const auto &i : dir_graph.topo_sort())
+        std::cout << i << ' ';
+    std::cout << '\n';
+    //     dir_graph.print();
+    //     dir_graph.dfs_trans(0);
+    //     dir_graph.bfs_trans(0);
+    //     ds::Trans(dir_graph, 0, ds::TransType::DFS);
+    //     auto undir_graph = ds::get_graph_undire();
+    //     undir_graph.print();
+    //     undir_graph.dfs_trans(1);
+    //     undir_graph.bfs_trans(0);
+    //     ds::Trans(undir_graph, 0, ds::TransType::BFS);
+    //     std::cout << dir_graph.bfs_arc_exist(1, 3) << std::endl;
+    //     std::cout << undir_graph.path_exist(3) << '\n';
+    //     std::cout << dir_graph.longest_path() << '\n';
+    //     ds::timer ti;
+    //     std::cout << undir_graph.path_exist(3) << '\n';
+    //     std::cout << ti.elapsed() << '\n';
+    //     ti.reset();
+    //     undir_graph.dfs_trans(0);
+    //     std::cout << ti.elapsed() << '\n';
+
+    auto dir_graph_Matrix = ds::get_graph_matrix_dire();
+    auto res = dir_graph_Matrix.topologicalSort();
+    // std::cout << dir_graph_Matrix.longest_path() << '\n';
+    //     std::cout << dir_graph_Matrix.exit_path_number(0, 4, 2) << std::endl;
+    // auto undir_list_graph = ds::get_graph_undire();
+    // undir_list_graph.print();
+    // std::cout << undir_list_graph.is_tree() << std::endl;
 }
