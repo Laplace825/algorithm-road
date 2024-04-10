8daf4dd8682fa6563753c5ef12a4be6f729e9fe2
laplace825 <2959452323@qq.com>
2024-04-10 19:43:09 +0800

    update ds diagram

--- Data-Structure/diagram/diagram.cpp
+++ Data-Structure/diagram/diagram.cpp
@@ -1,36 +1,38 @@
 #include <iostream>
 #include "diagram.hpp"
+#include <format>
 
-int main()
-{
-    auto dir_graph = ds::get_graph_dire();
-    for (const auto &i : dir_graph.topo_sort())
-        std::cout << i << ' ';
-    std::cout << '\n';
-    //     dir_graph.print();
-    //     dir_graph.dfs_trans(0);
-    //     dir_graph.bfs_trans(0);
-    //     ds::Trans(dir_graph, 0, ds::TransType::DFS);
-    //     auto undir_graph = ds::get_graph_undire();
-    //     undir_graph.print();
-    //     undir_graph.dfs_trans(1);
-    //     undir_graph.bfs_trans(0);
-    //     ds::Trans(undir_graph, 0, ds::TransType::BFS);
-    //     std::cout << dir_graph.bfs_arc_exist(1, 3) << std::endl;
-    //     std::cout << undir_graph.path_exist(3) << '\n';
-    //     std::cout << dir_graph.longest_path() << '\n';
-    //     ds::timer ti;
-    //     std::cout << undir_graph.path_exist(3) << '\n';
-    //     std::cout << ti.elapsed() << '\n';
-    //     ti.reset();
-    //     undir_graph.dfs_trans(0);
-    //     std::cout << ti.elapsed() << '\n';
+int main() {
+    auto dirListGraph = ds::get_graph_dire();
+    auto undirListGraph = ds::get_graph_undire();
+    auto undirMatrixGraph = ds::get_graph_matrix_dire();
 
-    auto dir_graph_Matrix = ds::get_graph_matrix_dire();
-    auto res = dir_graph_Matrix.topologicalSort();
-    // std::cout << dir_graph_Matrix.longest_path() << '\n';
-    //     std::cout << dir_graph_Matrix.exit_path_number(0, 4, 2) << std::endl;
-    // auto undir_list_graph = ds::get_graph_undire();
-    // undir_list_graph.print();
-    // std::cout << undir_list_graph.is_tree() << std::endl;
+    std::cout << "Dir List Graph\n";
+    dirListGraph.print();
+    std::cout << std::format("bfs_arc_exist(1,3){}\n",
+                             dirListGraph.bfs_arc_exist(1, 3));
+    std::cout << "========================================\n";
+
+    std::cout << "Undir List Graph\n";
+    undirListGraph.print();
+    std::cout << std::format("is tree?:{}\n", undirListGraph.is_tree());
+    std::cout << std::format("the {} length path exist:{}\n", 4, undirListGraph.path_exist(4));
+    std::cout << std::format("the {} length path exist:{}\n", 8, undirListGraph.path_exist(8));
+    std::cout << "=========================================\n";
+
+    std::cout << "Undir Matrix Graph\n";
+    std::cout << std::format("the {} length path exist between {} and {} is {}\n", 2, 1,
+                             4, undirMatrixGraph.exit_path_length(1, 4, 2));
+    std::cout << std::format("the {} length path exist between {} and {} is {}\n", 1, 1,
+                             4, undirMatrixGraph.exit_path_length(1, 4, 1));
+    std::cout << std::format("the longest path is {}\n", undirMatrixGraph.longest_path());
+    auto afterTopo = undirMatrixGraph.topologicalSort();
+    std::cout << "After Topological Sort\n";
+    std::for_each(
+            afterTopo.begin(), afterTopo.end(),
+            [](int i) { std::cout << i << " "; }
+    );
+    std::cout << "\n======================================\n";
+
+    return 0;
 }
