2e45368c4cfb872d59cb5846dcd63cf13deea273
Laplace825 <laplace_yongle_he@163.com>
2024-05-07 14:50:45 +0800

    just test

--- DS/AVLTree/AVLTree.hpp
+++ DS/AVLTree/AVLTree.hpp
@@ -9,16 +9,17 @@
 namespace BT {
 
 template <typename Elem>
-class AVLTree {
-   protected:
-    enum RotateType : short { LL = 1, RR = -1, LR = -1, RL = 1 };
-    struct AVLNode {
-        Elem data;            // 数据域
-        AVLNode *leftChild;   // 左孩子
-        AVLNode *rightChild;  // 右孩子
-        size_t height;        // 该节点的高度
-        short BF;             // 平衡因子
-        size_t lsize;         // 该结点左子树中结点数加1
+class AVLTree
+{
+protected:
+    struct AVLNode
+    {
+        Elem data;           // 数据域
+        AVLNode *leftChild;  // 左孩子
+        AVLNode *rightChild; // 右孩子
+        size_t height;       // 该节点的高度
+        short BF;            // 平衡因子
+        size_t lsize;        // 该结点左子树中结点数加1
 
         AVLNode() = default;
         AVLNode(const Elem &_data, size_t _height = 1,
@@ -29,14 +30,18 @@ class AVLTree {
               leftChild(_leftC),
               rightChild(_rightC),
               lsize(_lsize),
-              BF(0) {}
+              BF(0)
+        {
+        }
 
-        ~AVLNode() {
+        ~AVLNode()
+        {
             if (leftChild) delete leftChild;
             if (rightChild) delete rightChild;
         }
         // 重载输出运算符
-        friend std::ostream &operator<<(std::ostream &os, const AVLNode &node) {
+        friend std::ostream &operator<<(std::ostream &os, const AVLNode &node)
+        {
             return os << std::format("data[{}] height[{}] BF[{}] lsize[{}]",
                                      node.data, node.height, node.BF,
                                      node.lsize);
@@ -44,28 +49,34 @@ class AVLTree {
     };
     AVLNode *root;
 
-    size_t getHeight(AVLNode *node) const noexcept {
+    size_t getHeight(AVLNode *node) const noexcept
+    {
         return node ? node->height : 0;
     }
     short getBF(AVLNode *node) const noexcept { return node ? node->BF : 0; }
 
-    void updateBF(AVLNode *node) noexcept {
+    void updateBF(AVLNode *node) noexcept
+    {
         node->BF = getHeight(node->rightChild) - getHeight(node->leftChild);
     }
 
-    void updateHeight(AVLNode *node) noexcept {
+    void updateHeight(AVLNode *node) noexcept
+    {
         node->height =
             std::max(getHeight(node->leftChild), getHeight(node->rightChild)) +
             1;
     }
 
-    void updateStatus(AVLNode *node) noexcept {
+    void updateStatus(AVLNode *node) noexcept
+    {
         updateHeight(node);
         updateBF(node);
     }
 
-    void updateLsizeWhenRotate(AVLNode *node) noexcept {
-        if (node) {
+    void updateLsizeWhenRotate(AVLNode *node) noexcept
+    {
+        if (node)
+        {
             if (node->leftChild)
                 node->lsize = node->leftChild->lsize + 1;
             else
@@ -73,7 +84,8 @@ class AVLTree {
         }
     }
 
-    void LL_rotate(AVLNode *&subRoot) {
+    void LL_rotate(AVLNode *&subRoot)
+    {
         /**
          * @description: subRoot 表示需要旋转的子树根节点
          * @param {AVLNode} *subRoot
@@ -89,7 +101,8 @@ class AVLTree {
         subRoot = tmp;
     }
 
-    void RR_rotate(AVLNode *&subRoot) {
+    void RR_rotate(AVLNode *&subRoot)
+    {
         AVLNode *tmp = subRoot->rightChild;
         subRoot->rightChild = tmp->leftChild;
         tmp->leftChild = subRoot;
@@ -100,7 +113,8 @@ class AVLTree {
         subRoot = tmp;
     }
 
-    void RL_rotate(AVLNode *&subRoot) {
+    void RL_rotate(AVLNode *&subRoot)
+    {
         /**
          * @description: 先将subRoot的右孩子LL旋转,再将自己RR旋转
          * @param {AVLNode} *subRoot
@@ -110,19 +124,22 @@ class AVLTree {
         RR_rotate(subRoot);
     }
 
-    void LR_rotate(AVLNode *&subRoot) {
+    void LR_rotate(AVLNode *&subRoot)
+    {
         RR_rotate(subRoot->leftChild);
         LL_rotate(subRoot);
     }
 
-   public:
+public:
     AVLTree() : root{nullptr} {}
 
-    ~AVLTree() {
+    ~AVLTree()
+    {
         std::queue<AVLNode *> que;
         if (!root)
             ;
-        else {
+        else
+        {
             auto tmp = root;
             delete tmp;
             tmp->leftChild = nullptr;
@@ -130,23 +147,29 @@ class AVLTree {
         }
     }
 
-    void print() const noexcept {
+    void print() const noexcept
+    {
         /**
          * @note: 按照树形结构进行打印
          */
         auto printHelp = [](auto &&func, AVLNode *node, int indent) -> void {
-            if (node != nullptr) {
-                if (node->rightChild) {
+            if (node != nullptr)
+            {
+                if (node->rightChild)
+                {
                     func(func, node->rightChild, indent + 4);
                 }
-                if (indent) {
+                if (indent)
+                {
                     std::cout << std::setw(indent) << ' ';
                 }
-                if (node->rightChild) {
+                if (node->rightChild)
+                {
                     std::cout << " /\n" << std::setw(indent) << ' ';
                 }
                 std::cout << node->data << "\n ";
-                if (node->leftChild) {
+                if (node->leftChild)
+                {
                     std::cout << std::setw(indent) << ' ' << " \\\n";
                     func(func, node->leftChild, indent + 4);
                 }
@@ -156,7 +179,8 @@ class AVLTree {
         std::cout << std::endl;
     }
 
-    auto find(const Elem &key) {
+    auto find(const Elem &key)
+    {
         AVLNode *nearBFneq0Node{nullptr};
         AVLNode *PnearBFneq0Node{nullptr};
         AVLNode *PKeyNode{nullptr};
@@ -164,16 +188,22 @@ class AVLTree {
         if (!root)
             return std::make_tuple(curNode, PKeyNode, nearBFneq0Node,
                                    PnearBFneq0Node);
-        else {
-            while (curNode && curNode->data != key) {
-                if (curNode->BF != 0) {
+        else
+        {
+            while (curNode && curNode->data != key)
+            {
+                if (curNode->BF != 0)
+                {
                     PnearBFneq0Node = PKeyNode;
                     nearBFneq0Node = curNode;
                 }
-                if (key < curNode->data) {
+                if (key < curNode->data)
+                {
                     PKeyNode = curNode;
                     curNode = curNode->leftChild;
-                } else {
+                }
+                else
+                {
                     PKeyNode = curNode;
                     curNode = curNode->rightChild;
                 }
@@ -183,7 +213,8 @@ class AVLTree {
         }
     }
 
-    void insert(const Elem &e) noexcept {
+    void insert(const Elem &e) noexcept
+    {
         // @note:keyNode为e对应的节点,PKeyNode为keyNode双亲
         // nearBFneq0Node 为最近一个BF不等于0的,PnearBFneq0Node为前面的双亲
         auto abs = [](const auto &x) { return x > 0 ? x : -x; };
@@ -194,44 +225,58 @@ class AVLTree {
 
         auto insertHelp = [this, &e, &abs, &updateLsize](
                               auto &&func, AVLNode *&subRoot) -> void {
-            if (subRoot == nullptr) {
+            if (subRoot == nullptr)
+            {
                 subRoot = new AVLNode(e);
                 return;
-            } else if (subRoot->data > e) {
+            }
+            else if (subRoot->data > e)
+            {
                 // @note : 左子树插入
 
                 func(func, subRoot->leftChild);
                 updateStatus(subRoot);
                 updateLsize(subRoot);
-                if (abs(getBF(subRoot)) > 1) {
-                    if (getBF(subRoot->leftChild) == 1) {
+                if (abs(getBF(subRoot)) > 1)
+                {
+                    if (getBF(subRoot->leftChild) == 1)
+                    {
                         LR_rotate(subRoot);
-                    } else if (getBF(subRoot->leftChild) == -1) {
+                    }
+                    else if (getBF(subRoot->leftChild) == -1)
+                    {
                         LL_rotate(subRoot);
                     }
                 }
             }
 
-            else if (subRoot->data < e) {
+            else if (subRoot->data < e)
+            {
                 // @note: 右子树插入
 
                 func(func, subRoot->rightChild);
                 updateStatus(subRoot);
-                if (abs(getBF(subRoot)) > 1) {
-                    if (getBF(subRoot->rightChild) == 1) {
+                if (abs(getBF(subRoot)) > 1)
+                {
+                    if (getBF(subRoot->rightChild) == 1)
+                    {
                         RR_rotate(subRoot);
-                    } else if (getBF(subRoot->rightChild) == -1) {
+                    }
+                    else if (getBF(subRoot->rightChild) == -1)
+                    {
                         RL_rotate(subRoot);
                     }
                 }
-            } else
-                return;  // @note: 已经存在相同元素,不插入
+            }
+            else
+                return; // @note: 已经存在相同元素,不插入
         };
 
         insertHelp(insertHelp, root);
     }
 
-    auto findTheKMinElem(size_t k) const noexcept {
+    auto findTheKMinElem(size_t k) const noexcept
+    {
         /**
          * @description: 找到第k小的元素
          * @param {size_t} k
@@ -240,9 +285,11 @@ class AVLTree {
         auto findtheKMinElemHelp = [](auto &&func, AVLNode *subRoot,
                                       int64_t iter) -> AVLNode * {
             // subRoot非空时
-            if (subRoot) {
+            if (subRoot)
+            {
                 // 左边的节点数多一些
-                if (subRoot->lsize > iter) {
+                if (subRoot->lsize > iter)
+                {
                     /**
                      * @note: 每次将iter-subRoot->lsize检查当前被跳过的元素个数
                      */
@@ -250,8 +297,9 @@ class AVLTree {
                                ? iter - subRoot->lsize
                                : iter;
                     return func(func, subRoot->leftChild, iter);
-
-                } else if (subRoot->lsize < iter) {
+                }
+                else if (subRoot->lsize < iter)
+                {
                     iter = iter - static_cast<int64_t>(subRoot->lsize) > 0
                                ? iter - subRoot->lsize
                                : iter;
@@ -264,7 +312,8 @@ class AVLTree {
         return findtheKMinElemHelp(findtheKMinElemHelp, root, k);
     }
 
-    size_t height() const noexcept {
+    size_t height() const noexcept
+    {
         size_t heightAvlTree{0};
         // while (curNode) {
         //     if (curNode->BF > 0) {
@@ -280,7 +329,8 @@ class AVLTree {
         auto heightHelp = [&heightAvlTree](auto &&func, AVLNode *node) {
             if (!node)
                 return heightAvlTree;
-            else {
+            else
+            {
                 heightAvlTree++;
                 // @note: 树左边高则BF<0
                 return node->BF >= 0 ? func(func, node->rightChild)
@@ -292,5 +342,5 @@ class AVLTree {
     }
 };
 
-}  // namespace BT
-#endif  // __AVL_TREE_HPP__
+} // namespace BT
+#endif // __AVL_TREE_HPP__
--- DS/AVLTree/main.cpp
+++ DS/AVLTree/main.cpp
@@ -14,18 +14,18 @@
 #include "AVLTree.hpp"
 
 signed main() {
-    BT::AVLTree<int> AvlTr;
-    constexpr int arr[] = {50, 30, 53, 52, 55, 26, 33, 10, 27, 28};
-    for (size_t i = 0; i < sizeof(arr) / 4; i++) {
-        AvlTr.insert(arr[i]);
-    }
-    std::cout << "*-----------\033[1;33mAvlTree\033[0m------------*\n";
-    AvlTr.print();
-    std::cout << "--------------------------------\n";
-    std::cout << "find the \033[1;33m2nd\033[0m smallest :"
-              << *AvlTr.findTheKMinElem(2) << '\n';
-    std::cout << "find the \033[1;33m7th\033[0m smallest :"
-              << *AvlTr.findTheKMinElem(7) << '\n';
-    std::cout << "--------------------------------\n";
-    std::cout << std::format("use BF to calculate height:{}\n", AvlTr.height());
+  BT::AVLTree<int> AvlTr;
+  constexpr int arr[] = {50, 30, 53, 52, 55, 26, 33, 10, 27, 28};
+  for (size_t i = 0; i < sizeof(arr) / 4; i++) {
+    AvlTr.insert(arr[i]);
+  }
+  std::cout << "*-----------\033[1;33mAvlTree\033[0m------------*\n";
+  AvlTr.print();
+  std::cout << "--------------------------------\n";
+  std::cout << "find the \033[1;33m2nd\033[0m smallest :"
+            << *AvlTr.findTheKMinElem(2) << '\n';
+  std::cout << "find the \033[1;33m7th\033[0m smallest :"
+            << *AvlTr.findTheKMinElem(7) << '\n';
+  std::cout << "--------------------------------\n";
+  std::cout << std::format("use BF to calculate height:{}\n", AvlTr.height());
 }
